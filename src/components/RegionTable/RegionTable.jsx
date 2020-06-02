aa6f0748e46636896423efe4dcf3dfd745540627
kamlesh <kamlesh@kamleshs-MacBook-Air.local>
2020-06-03 01:54:34 +0530

    align table cell center

--- src/components/RegionTable/RegionTable.jsx
+++ src/components/RegionTable/RegionTable.jsx
@@ -20,12 +20,12 @@ const RegionTable = ({ globalData, indianData, onRegionChange }) => {
               <TableHead>
                 <TableRow>
                   <TableCell>Region</TableCell>
-                  <TableCell align="right" className={styles.confirmed}>Confirmed</TableCell>
-                  <TableCell align="right" className={styles.recovered}>Recovered</TableCell>
-                  <TableCell align="right" className={styles.deaths}>Deaths</TableCell>
-                  <TableCell align="right" className={styles.active}>Active</TableCell>
-                  <TableCell align="right" className={styles.infoRecovery}>Reovery Rate(%)</TableCell>
-                  <TableCell align="right" className={styles.infoMortality}> Mortality Rate(%)</TableCell>
+                  <TableCell align="center" className={styles.confirmed}>Confirmed</TableCell>
+                  <TableCell align="center" className={styles.recovered}>Recovered</TableCell>
+                  <TableCell align="center" className={styles.deaths}>Deaths</TableCell>
+                  <TableCell align="center" className={styles.active}>Active</TableCell>
+                  <TableCell align="center" className={styles.infoRecovery}>Reovery (%)</TableCell>
+                  <TableCell align="center" className={styles.infoMortality}> Mortality (%)</TableCell>
                   <TableCell align="right"> Last Update</TableCell>
                 </TableRow>
               </TableHead>
@@ -34,12 +34,12 @@ const RegionTable = ({ globalData, indianData, onRegionChange }) => {
                 {globalData.confirmed && (
                   <TableRow key={1}>
                     <TableCell><CountryPicker handleChange={handleCountryChange} /></TableCell>
-                    <TableCell align="right" className={styles.confirmed}><CountUp start={0} end={globalData.confirmed} duration={2} separator="," gutterBottom /></TableCell>
-                    <TableCell align="right" className={styles.recovered}><CountUp start={0} end={globalData.recovered} duration={2} separator="," gutterBottom /></TableCell>
-                    <TableCell align="right" className={styles.deaths}><CountUp start={0} end={globalData.deaths} duration={2} separator="," gutterBottom /></TableCell>
-                    <TableCell align="right" className={styles.active}><CountUp start={0} end={globalData.active} duration={2} separator="," gutterBottom /></TableCell>
-                    <TableCell align="right" className={styles.infoRecovery}><CountUp start={0} end={globalData.recoveryRate} duration={2} separator="," decimals={2} gutterBottom /></TableCell>
-                    <TableCell align="right" className={styles.infoMortality}><CountUp start={0} end={globalData.mortalityRate} duration={2} separator="," decimals={2} gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.confirmed}><CountUp start={0} end={globalData.confirmed} duration={2} separator="," gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.recovered}><CountUp start={0} end={globalData.recovered} duration={2} separator="," gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.deaths}><CountUp start={0} end={globalData.deaths} duration={2} separator="," gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.active}><CountUp start={0} end={globalData.active} duration={2} separator="," gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.infoRecovery}><CountUp start={0} end={globalData.recoveryRate} duration={2} separator="," decimals={2} gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.infoMortality}><CountUp start={0} end={globalData.mortalityRate} duration={2} separator="," decimals={2} gutterBottom /></TableCell>
                     <TableCell align="right"> {new Date(globalData.lastUpdate).toLocaleString()} </TableCell>
                   </TableRow>
                 )}
@@ -47,12 +47,12 @@ const RegionTable = ({ globalData, indianData, onRegionChange }) => {
                 {indianData.confirmed && (
                   <TableRow key={2}>
                     <TableCell><CountryPicker type="state" handleChange={handleStateChange} /></TableCell>
-                    <TableCell align="right" className={styles.confirmed}><CountUp start={0} end={indianData.confirmed} duration={2} separator="," gutterBottom /></TableCell>
-                    <TableCell align="right" className={styles.recovered}><CountUp start={0} end={indianData.recovered} duration={2} separator="," gutterBottom /></TableCell>
-                    <TableCell align="right" className={styles.deaths}><CountUp start={0} end={indianData.deaths} duration={2} separator="," gutterBottom /></TableCell>
-                    <TableCell align="right" className={styles.active}><CountUp start={0} end={indianData.active} duration={2} separator="," gutterBottom /></TableCell>
-                    <TableCell align="right" className={styles.infoRecovery}><CountUp start={0} end={indianData.recoveryRate} duration={2} separator="," decimals={2} gutterBottom /></TableCell>
-                    <TableCell align="right" className={styles.infoMortality}><CountUp start={0} end={indianData.mortalityRate} duration={2} separator="," decimals={2} gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.confirmed}><CountUp start={0} end={indianData.confirmed} duration={2} separator="," gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.recovered}><CountUp start={0} end={indianData.recovered} duration={2} separator="," gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.deaths}><CountUp start={0} end={indianData.deaths} duration={2} separator="," gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.active}><CountUp start={0} end={indianData.active} duration={2} separator="," gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.infoRecovery}><CountUp start={0} end={indianData.recoveryRate} duration={2} separator="," decimals={2} gutterBottom /></TableCell>
+                    <TableCell align="center" className={styles.infoMortality}><CountUp start={0} end={indianData.mortalityRate} duration={2} separator="," decimals={2} gutterBottom /></TableCell>
                     <TableCell align="right"> {new Date(indianData.lastUpdate).toLocaleString()} </TableCell>
                   </TableRow>
                 )}
